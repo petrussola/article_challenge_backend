5525505e7c34515b900ffbd0362493d9694a3dd4
Pere Solà Claver <pere.solaclaver@gmail.com>
2021-03-14 16:47:48 +0000

    Seed authors table

--- scripts/seed.js
+++ scripts/seed.js
@@ -1,12 +1,16 @@
-import sequelize from '../src/connectors/sequelize';
-import models from '../src/models';
+import sequelize from "../src/connectors/sequelize";
+import models from "../src/models";
 
-const { Region } = models;
+const { Region, Author } = models;
 
 (async () => {
-    await sequelize.sync({ force: true });
+  await sequelize.sync({ force: true });
 
-    Region.create({ code: 'AU', name: 'Australia' });
-    Region.create({ code: 'UK', name: 'United Kingdom' });
-    Region.create({ code: 'US', name: 'United States of America' });
+  Region.create({ code: "AU", name: "Australia" });
+  Region.create({ code: "UK", name: "United Kingdom" });
+  Region.create({ code: "US", name: "United States of America" });
+  Author.create({ firstName: "Agatha", lastName: "Christie" });
+  Author.create({ firstName: "John", lastName: "le Carré" });
+  Author.create({ firstName: "J.K.", lastName: "Rowling" });
+  Author.create({ firstName: "Amin", lastName: "Maalouf" });
 })();
